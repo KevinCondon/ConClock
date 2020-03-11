2613dc9c1b76cc8eed06f336887b66b4430e3f1f
Kevin Condon <kcondon@alleni.com>
2020-03-11 15:52:17 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import Clock from 'react-live-clock';
 
+//var nextRadioTime = "1 am";
+//var nextDucklingTime = "2 am";
+//var nextBridgeTime = "3 am";
+var currentTime = Date();
+
+const eventTime = new Date('January 23, 2020 16:05:00')
+
+//eventTime.setMinutes(12);
+var nextRadioHour = eventTime.getHours();
+var nextRadioMinutes = eventTime.getMinutes();
+// this isn't working for some reason // var currentHour = currentTime.getHours();
+
+
+console.log(currentTime);
+console.log(eventTime);
+console.log(nextRadioHour);
+console.log(nextRadioMinutes);
+//console.log (new Date());
+console.log (currentTime);
+
+/*
+if (currentHour === nextRadioHour){
+  //console.log("Yo, it's time!");
+}
+*/
 function App() {
-  return (
+  return (        
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <header className="App-time">
+          <Clock           
+          format={'h:mm:ss'} 
+          ticking={true} 
+          timezone={'US/Central'}          
+          />  
+         
+         </header>  
+        <header className="App-ampm">
+          
+        <Clock format={'a'}       
+        /> 
+             
+        </header>
+    </div>      
   );
 }
+/*
+
+ <Clock
+          format={' a'}
+          />
+Next Radio Training: { nextRadioHour}:{ nextRadioMinutes } 
+*/
 
 export default App;
